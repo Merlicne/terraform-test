86114ca7963b9835f212493a9531a14c57e18f25
Merlicne <phannakon.p@kkumail.com>
2026-03-27 23:30:19 +0700

    add otel

--- gcp/04-dns/internal_records/main.tf
+++ gcp/04-dns/internal_records/main.tf
@@ -63,3 +63,11 @@ resource "google_dns_record_set" "tempo" {
   ttl          = 300
   rrdatas      = [local.gateway_ip]
 }
+
+resource "google_dns_record_set" "otel_collector" {
+  name         = "otel-collector.${data.google_dns_managed_zone.private_zone.dns_name}"
+  managed_zone = data.google_dns_managed_zone.private_zone.name
+  type         = "A"
+  ttl          = 300
+  rrdatas      = [local.gateway_ip]
+}
